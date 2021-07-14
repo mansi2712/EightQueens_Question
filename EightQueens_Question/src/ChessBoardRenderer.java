09bef4d331f45ffa20d205476fd4bd394d306c5f
Dell <Dell@Mansi>
2021-07-14 12:56:24 +0530

    Final commit

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,17 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
+		if((square/8)%2!=0)
+		{
+			if(square%2==0)
+				return true;
+		}
+		else if((square/8)%2==0)
+		{
+			if(square%2!=0)
+				return true;
+		}
 		System.out.println(square);
-		return true;
+		return false;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -5,13 +5,15 @@ public class GameStrategy {
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
 
-		return 0;	// just for the heck of it
+		return cellId%8;
+//		return 0;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
 		
-		return 0;	// just for the heck of it
+		return cellId/8;
+//		return 0;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -29,6 +31,43 @@ public class GameStrategy {
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
+		if(placedQueens[row][col]==true){
+			return false;
+		}
+		else
+		{
+			//if already in row or column
+			for(int i=0;i<8;i++)
+			{
+				if(placedQueens[row][i] || placedQueens[i][col])
+					return false;
+			}
+			//in diagnols
+			for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--)
+			{
+				if(placedQueens[i][j])
+					return false;
+			}
+			for(int i=row-1,j=col+1;i>=0 && j<8;i--,j++)
+			{
+				if(placedQueens[i][j])
+					return false;
+			}
+			for(int i=row+1,j=col-1;i<8 && j>=0;i++,j--)
+			{
+				if(placedQueens[i][j])
+					return false;
+			}
+			for(int i=row+1,j=col+1;i<8 && j<8;i++,j++)
+			{
+				if(placedQueens[i][j])
+					return false;
+			}
+			
+		}
+		//if false not returned from any condition
+		placedQueens[row][col]=true;
+		numQueens++;
 		return isValid;
 	}
 }
